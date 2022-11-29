cfcba75a172432dd6198cb87a826553981e1c697
furkantok <furkant.ftk@gmail.com>
2022-11-29 12:07:11 +0300

    question_1 cr'ye göre düzenlendi

--- functions/question_1.py
+++ functions/question_1.py
@@ -6,18 +6,16 @@
 
 tam_bolunenler = []
 
-min_input = int(input("Minimum sayı değerini girin: "))
-max_input = int(input("Maximum sayı değerini girin: "))
-bolen_input = int(input("Bölen sayı değerini girin: "))
+min_sayi = int(input("Minimum sayı değerini girin: "))
+max_sayi = int(input("Maximum sayı değerini girin: "))
+bolen_sayi = int(input("Bölen sayı değerini girin: "))
 
 
-def bolunen_sayı_bulma(min_sayi = min_input, max_sayi = max_input, bolen_sayi = bolen_input):
-
+def bolunen_sayı_bulma(min_sayi, max_sayi, bolen_sayi):
     for her_sayi in range(min_sayi, max_sayi + 1):
-
         if her_sayi % bolen_sayi == 0:
             tam_bolunenler.append(her_sayi)
-
     return tam_bolunenler
 
-print(bolunen_sayı_bulma())
+
+print(bolunen_sayı_bulma(min_sayi, max_sayi, bolen_sayi))
